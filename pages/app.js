789f7d795a82964a153870c18d85f4d1a2b63754
vveeddaannttEDM <119670953+vveeddaannttEDM@users.noreply.github.com>
2025-03-11 12:58:57 +0530

    Create app.js

---
+++ pages/app.js
@@ -0,0 +1,21 @@
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { useMemo } from "react";
+import "../styles/globals.css";
+
+const App = ({ Component, pageProps }) => {
+  const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint="https://api.devnet.solana.com">
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default App;
